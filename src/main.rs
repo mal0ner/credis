f78bb85940b0ed29604f5e8f254ac2cc3c00189f
Cameron Malone <cameronmkm2@gmail.com>
2024-05-27 14:39:54 +1000

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,30 @@
 mod command;
 mod protocol;
 // Uncomment this block to pass the first stage
-
+use clap::Parser;
+use clap_num::number_range;
 use std::{
     collections::HashMap,
     sync::{Arc, Mutex},
 };
-
 use tokio::net::TcpListener;
+
+fn port_range(s: &str) -> Result<u16, String> {
+    number_range(s, 1024, 65535)
+}
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Number of times to greet
+    #[arg(long, default_value_t = 6379, value_parser=port_range)]
+    port: u16,
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<(), anyhow::Error> {
-    let listener = TcpListener::bind("127.0.0.1:6379").await?;
+    let args = Args::parse();
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", args.port)).await?;
 
     let cache = Arc::new(Mutex::new(HashMap::new()));
     loop {
